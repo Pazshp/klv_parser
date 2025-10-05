58b25f7cd5b8656d6c4f685d2abdf509dce07f1b
Mr.Piz <paz.shpilman@axon-vision.com>
2025-10-05 15:34:42 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='v0.0.5',
+    version='v0.0.6',
 
     description='A Python library for parsing MISB/STANAG 4609 Key Length Value (KLV) metadata.',
     long_description=long_description,
@@ -68,6 +68,8 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: 3',
     ],
 
